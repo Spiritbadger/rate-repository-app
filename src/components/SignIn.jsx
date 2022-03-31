6b671fb8b4a416b1fff52412be713ed490528bf0
Jan Nyman <jan.r.nyman@gmail.com>
2022-03-31 18:17:05 +0300

    Form validation for sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,21 @@
 import { Text, Pressable, View, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import FormikTextInput from './FormikTextInput';
+import * as yup from 'yup';
 
 const initialValues = {
   username: '',
   password: '',
 };
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Username is required'),
+  password: yup
+    .string()
+    .required('Password is required'),
+});
 
 const styles = StyleSheet.create({
   bigButton: {
@@ -45,7 +55,11 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+    validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,17 @@
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  errorInputField: {
+    borderColor: '#d73a4a',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 3,
+    margin: 5
+  },
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = (error ? styles.errorInputField: [style]);
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
